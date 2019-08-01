bb7f3909543f020e0e64180cd2686ff1da34106e
tjblackheart <git@subdev.org>
2019-08-01 08:59:29 +0200

    add generator test

--- app/pkg/pdf/generator_test.go
+++ app/pkg/pdf/generator_test.go
@@ -5,6 +5,7 @@ import (
 	"path"
 	"regexp"
 	"runtime"
+	"strings"
 	"testing"
 	"time"
 
@@ -15,22 +16,32 @@ import (
 var mockInvoice = models.Invoice{
 	Number:     "T0001",
 	Date:       time.Now(),
-	TotalNet:   100.00,
-	TotalGross: 119.00,
+	TotalNet:   money.Money(10000),
+	TotalGross: money.Money(11900),
 	Currency:   "EUR",
 	Items: []models.InvoiceItem{
 		models.InvoiceItem{
 			Description:  "Testitem",
 			Amount:       2,
 			Unit:         "hrs",
-			PricePerUnit: money.Money(50),
+			PricePerUnit: money.Money(5000),
 			VAT:          19.0,
-			InvoiceID:    1,
 		},
 	},
-	IsCancelled: false,
-	IsPaid:      false,
-	Customer:    models.Customer{},
+	Customer: models.Customer{
+		Number:    "C100",
+		TaxNumber: "TAX123",
+		Address: models.Address{
+			Company:   "Test Corp",
+			FirstName: "Cus",
+			LastName:  "Tomer",
+			Street:    "Customerstreet",
+			Number:    "999",
+			Zip:       "11111",
+			City:      "City",
+			Country:   "Germany",
+		},
+	},
 }
 
 var mockUser = models.User{
@@ -42,14 +53,13 @@ var mockUser = models.User{
 		Number:    "123a",
 		City:      "Testing",
 		Zip:       "12345",
-		Bank:      "Testbank",
-		IBAN:      "TT1234",
-		BIC:       "TESTFF",
 		TaxNumber: "TEST465",
 		Email:     "test@test.com",
-		Phone:     "12345",
+		Phone:     "01234567",
 	},
 }
+
+var g = Generator{Invoice: &mockInvoice, User: &mockUser}
 
 // https://brandur.org/fragments/testing-go-project-root
 func init() {
@@ -61,7 +71,6 @@ func init() {
 }
 
 func TestGenerate(t *testing.T) {
-	g := Generator{Invoice: &mockInvoice, User: &mockUser}
 	fname, err := g.Generate()
 	if err != nil {
 		t.Errorf("%s", err)
@@ -80,7 +89,6 @@ func TestGenerate(t *testing.T) {
 }
 
 func TestBase64(t *testing.T) {
-	g := Generator{Invoice: &mockInvoice, User: &mockUser}
 	fname, err := g.Generate()
 	if err != nil {
 		t.Errorf("%s", err)
@@ -96,5 +104,69 @@ func TestBase64(t *testing.T) {
 	matched, _ := regexp.MatchString(`^([A-Za-z0-9+/]{4})*([A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}==)?$`, b64)
 	if matched == false {
 		t.Errorf("not a Base64 encoded string: %s", b64)
+	}
+}
+
+func TestHtml(t *testing.T) {
+	html, err := g.toHTML()
+	if err != nil {
+		t.Errorf("%s", err)
+		t.FailNow()
+	}
+
+	tests := []struct {
+		needle string
+	}{
+		{mockInvoice.Number},
+		{mockInvoice.Date.Format("02.01.2006")},
+		{mockInvoice.TotalNet.Format()},
+		{mockInvoice.TotalGross.Format()},
+		{mockInvoice.Currency},
+		{mockInvoice.Customer.TaxNumber},
+		{mockInvoice.Customer.Address.Company},
+		{mockInvoice.Customer.Address.FirstName},
+		{mockInvoice.Customer.Address.LastName},
+		{mockInvoice.Customer.Address.Street},
+		{mockInvoice.Customer.Address.City},
+		{mockInvoice.Customer.Address.Zip},
+		{mockUser.Settings.Company},
+		{mockUser.Settings.FirstName},
+		{mockUser.Settings.LastName},
+		{mockUser.Settings.City},
+		{mockUser.Settings.Street},
+		{mockUser.Settings.Zip},
+	}
+
+	for _, tt := range tests {
+		if strings.Contains(html, tt.needle) == false {
+			t.Errorf("want string: %s - not found", tt.needle)
+		}
+	}
+}
+
+func TestFooter(t *testing.T) {
+	g.generateFooter()
+
+	tests := []struct {
+		haystack string
+		needle   string
+	}{
+		{g.footer.Left, mockUser.Settings.FirstName},
+		{g.footer.Left, mockUser.Settings.LastName},
+		{g.footer.Left, mockUser.Settings.Street},
+		{g.footer.Left, mockUser.Settings.Number},
+		{g.footer.Left, mockUser.Settings.Zip},
+		{g.footer.Center, mockUser.Settings.Bank},
+		{g.footer.Center, mockUser.Settings.IBAN},
+		{g.footer.Center, mockUser.Settings.BIC},
+		{g.footer.Right, mockUser.Settings.TaxNumber},
+		{g.footer.Right, mockUser.Settings.Email},
+		{g.footer.Right, mockUser.Settings.Phone},
+	}
+
+	for _, tt := range tests {
+		if strings.Contains(tt.haystack, tt.needle) == false {
+			t.Errorf("have: %s, want: %s", tt.haystack, tt.needle)
+		}
 	}
 }
